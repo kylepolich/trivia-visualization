c1261e97a3439fde90db11cae9134e98b580485b
Kyle Polich <kyle@dataskeptic.com>
2023-03-03 15:39:01 -0800

    updates

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 aws s3 cp index.html s3://dataskeptic.com/trivia-graph/ --acl public-read
-aws s3 cp team.json s3://dataskeptic.com/trivia-graph/team7.json --acl public-read
+aws s3 cp team.json s3://dataskeptic.com/trivia-graph/team8.json --acl public-read
 aws s3 cp alchemy.css s3://dataskeptic.com/trivia-graph/ --acl public-read
